311f3611abbbd79cf5b7f90fcc3e22437d5cc967
ax <15692413487@163.com>
2020-12-16 10:31:10 +0800

    'ax'

--- push.py
+++ push.py
@@ -3,18 +3,20 @@
 import os
 import sys
 
-type = sys.getfilesystemencoding()  # 获取系统默认编码类型
-print('===开始执行===').decode('utf-8').encode(type)  # 对打印内容先解码再编码
-print('===hexo clean .===')
-os.system("hexo clean")
-print('===hexo g===')
+print('===开始执行===')
 os.system("hexo g")
 print('===git add .===')
 os.system("git add .")
 print("===git commit -am 'ax'===")
 os.system("git commit -am 'ax'")
-print('===git push===')
-os.system("git push origin hexo")
-print('===hexo d===')
-os.system("hexo d")
-print('===执行结束===').decode('utf-8').encode(type)  # 对打印内容先解码再编码
+print('===git push origin master===')
+os.system("git push origin master")
+print('===cd public===')
+os.chdir('.\\public')
+print('===git add .===')
+os.system("git add .")
+print("===git commit -am 'ax'===")
+os.system("git commit -am 'ax'")
+print('===git push origin master===')
+os.system("git push origin master")
+print('===执行结束===')

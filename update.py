364d7c958f86d701fd28bcfb430cdc1fa1d8985f
xing.guo <xing.guo@vixtel.com>
2021-08-24 10:41:37 +0800

    'ax'

--- update.py
+++ update.py
@@ -5,14 +5,14 @@ import sys
 type = sys.getfilesystemencoding()     #获取系统默认编码类型
 
 print('===开始执行===').decode('utf-8').encode(type)    #对打印内容先解码再编码
-print('===git pull origin master===')
-os.system("git pull origin master")
-print('===git merge remoter/origin/master===')
-os.system("git merge remoter/origin/master")
+print('===git pull origin main===')
+os.system("git pull origin main")
+print('===git merge remoter/origin/main===')
+os.system("git merge remoter/origin/main")
 print('===cd public===')
 os.chdir('./public')
-print('===git pull origin master===')
-os.system("git pull origin master")
-print('===git merge remoter/origin/master===')
-os.system("git merge remoter/origin/master")
+print('===git pull origin main===')
+os.system("git pull origin main")
+print('===git merge remoter/origin/main===')
+os.system("git merge remoter/origin/main")
 print('===执行结束===').decode('utf-8').encode(type)    #对打印内容先解码再编码
